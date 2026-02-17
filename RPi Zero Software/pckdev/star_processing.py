d5320abe11556aabc230ea8d8e30cff306147b95
PecaJosef <pecka.jozka@seznam.cz>
2026-02-18 00:03:34 +0100

    Updated image processing to reduce noise and overall brightness impact

--- RPi Zero Software/pckdev/star_processing.py
+++ RPi Zero Software/pckdev/star_processing.py
@@ -11,25 +11,40 @@ ov_cephei_vectors = np.load("star_vectors/ov_cephei_vectors.npy")
 default_pix_per_arcmin = 4608/(14.334*60) #Default pixels per arminute - Later load from config file
 
 stars_to_find = 35
-stars_threshold = 30
-stars_to_loop_through = 8
+stars_threshold = 28
+stars_to_loop_through = 10
 
-def brightest_stars(number_of_stars, image):
+def image_preprocessing(image):
   gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+  #Get mean pixel value from the image
+  mean = np.mean(gray)
+
+  #Increase the image contrast based on mean pixel value
+  contrast_img = cv2.convertScaleAbs(gray, alpha=1, beta=-mean)
+  
   # Apply Gaussian blur to reduce noise
-  blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-  # Get threshold
+  blurred = cv2.GaussianBlur(contrast_img, (5, 5), 0)
+  
+  # Get threshold from contrasted and blurred image
   thresh, _ = cv2.threshold(blurred, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-  #print(thresh)
-  # Constrain threshold manually
+  
+  # Limit the threshold
   min_thresh = 25
   max_thresh = 100
   threshold = max(min_thresh, min(thresh, max_thresh))
   #print(threshold)
+
   # Apply adaptive thresholding to detect bright spots
   _, thresholded = cv2.threshold(blurred, threshold, 255, cv2.THRESH_BINARY) #If pix value < threshold  --->  turns pixel black
+
+  cv2.imwrite('thresholded.jpg', thresholded)
+
+  return thresholded
+
+def brightest_stars(number_of_stars, image):
   # Find contours (potential stars)
-  contours, _ = cv2.findContours(thresholded, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+  contours, _ = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
   # Filter out small contours based on area (to avoid noise)
   min_area = 1  # Minimum area for contours to be considered as stars
   contours = [cnt for cnt in contours if cv2.contourArea(cnt) > min_area]
@@ -83,7 +98,7 @@ def find_polaris(center_star, surrounding_stars, matched_vectors_threshold, star
         best_matches_per_vector = np.max(dot_matrix, axis=1)
         num_matching_pairs = np.count_nonzero(best_matches_per_vector > cos_threshold)
 
-        if num_matching_pairs >= 30:
+        if num_matching_pairs >= stars_threshold:
           matching_angles.append(angle)
           print("Angle:", angle, "Matching Pairs:", num_matching_pairs)
 
@@ -132,7 +147,7 @@ def check_star(center_star, surrounding_stars, matched_vectors_threshold, star_p
       best_matches_per_vector = np.max(dot_matrix, axis=1)
       num_matching_pairs = np.count_nonzero(best_matches_per_vector > cos_threshold)
 
-      if num_matching_pairs >= 15:
+      if num_matching_pairs >= stars_threshold:
         matching_angles.append(angle)
         print("Angle:", angle, "Matching Pairs:", num_matching_pairs)
 
@@ -264,72 +279,76 @@ def getAlignmentError(image):
   if image is None:
       print("Error: Could not load image. Please check the file path.")
       #Error should be sent to PCK main board here
-  else:
-    #Gets centers of brightest stars
-    star_centers = brightest_stars(stars_to_find, image) #Number of stars to be found, image
-    #print(star_centers.shape) #just for debugging to see how many stars were found
-    if (star_centers.size != stars_to_find): #Return if the number of stars is lower than the required amount
       return 0,0,False,False
+  
+  #Preprocess the image
+  preprocessed_image = image_preprocessing(image)
+  #Gets centers of brightest stars
+  star_centers = brightest_stars(stars_to_find, preprocessed_image) #Number of stars to be found, image
+  #print(star_centers.shape) #just for debugging to see how many stars were found
+  stars_found = len(star_centers) if star_centers is not None else 0
+  if (stars_found != stars_to_find): #Return if the number of stars is lower than the required amount
+    return 0,0,False,False
 
-    polaris_coordinates = np.array([])
-    yildun_coordinates = np.array([])
-    ov_cephei_coordinates = np.array([])
+  polaris_coordinates = np.array([])
+  yildun_coordinates = np.array([])
+  ov_cephei_coordinates = np.array([])
+
+  for index, star in enumerate(star_centers[:stars_to_loop_through]):
+    polarisFound, img_angle = find_polaris(star, np.append(star_centers[:index],star_centers[(index+1):], axis=0), stars_threshold, polaris_vectors) #Check if current star is Polaris and at which angle the Polaris was found compared to the vector set
+    if polarisFound == True:
+      print("Polaris is in the image at: X =", star[0], "Y =", star[1])
+      polaris_coordinates = star
+      break
+  print("IMG is at angle:", img_angle)
+
+  if polaris_coordinates.size != 0:
+    for index, star in enumerate(star_centers[:stars_to_loop_through]):
+      if check_star(star, np.append(star_centers[:index],star_centers[(index+1):], axis=0), stars_threshold, yildun_vectors, img_angle): #Checks if current star is Yildun
+        print("Yildun is in the image at: X =", star[0], "Y =", star[1])
+        yildun_coordinates = star
+        break
 
     for index, star in enumerate(star_centers[:stars_to_loop_through]):
-      polarisFound, img_angle = find_polaris(star, np.append(star_centers[:index],star_centers[(index+1):], axis=0), stars_threshold, polaris_vectors) #Check if current star is Polaris and at which angle the Polaris was found compared to the vector set
-      if polarisFound == True:
-        print("Polaris is in the image at: X =", star[0], "Y =", star[1])
-        polaris_coordinates = star
+      #print(index)
+      #print(star_centers[:index])
+      if check_star(star, np.append(star_centers[:index],star_centers[(index+1):], axis=0), stars_threshold, ov_cephei_vectors, img_angle): #Checks if current star is OV Cephei
+        print("OV Cephei is in the image at: X =", star[0], "Y =", star[1])
+        ov_cephei_coordinates = star
         break
-    print("IMG is at angle:", img_angle)
+  else:
+    print("Polaris not found")
+    polarisFound = False
+    #Error should be sent to PCK main board
 
-    if polaris_coordinates.size != 0:
-      for index, star in enumerate(star_centers[:stars_to_loop_through]):
-        if check_star(star, np.append(star_centers[:index],star_centers[(index+1):], axis=0), stars_threshold, yildun_vectors, img_angle): #Checks if current star is Yildun
-          print("Yildun is in the image at: X =", star[0], "Y =", star[1])
-          yildun_coordinates = star
-          break
+  ncp_error = np.array([0,0])
 
-      for index, star in enumerate(star_centers[:stars_to_loop_through]):
-        #print(index)
-        #print(star_centers[:index])
-        if check_star(star, np.append(star_centers[:index],star_centers[(index+1):], axis=0), stars_threshold, ov_cephei_vectors, img_angle): #Checks if current star is OV Cephei
-          print("OV Cephei is in the image at: X =", star[0], "Y =", star[1])
-          ov_cephei_coordinates = star
-          break
-    else:
-      print("Polaris not found")
-      polarisFound = False
-      #Error should be sent to PCK main board
+  #Distance between Polaris and Yildun in pixels
+  if polaris_coordinates.size != 0 and yildun_coordinates.size != 0 and ov_cephei_coordinates.size !=0: #Checks if all the stars were found
+    polaris_yildun_dis = np.linalg.norm(polaris_coordinates - yildun_coordinates)
+    polaris_ov_cephei_dis = np.linalg.norm(polaris_coordinates - ov_cephei_coordinates)
+    yildun_ov_cephei_dis = np.linalg.norm(yildun_coordinates - ov_cephei_coordinates)
 
-    ncp_error = np.array([0,0])
+    allStarsFound = True
+    #print("\nPolaris Yildun distance in pixels:", f"{polaris_yildun_dis:.2f}")
+    #print("Polaris OV Cephei distance in pixels:", f"{polaris_ov_cephei_dis:.2f}")
+    #print("Yildun OV Cephei distance in pixels:", f"{yildun_ov_cephei_dis:.2f}")
 
-    #Distance between Polaris and Yildun in pixels
-    if polaris_coordinates.size != 0 and yildun_coordinates.size != 0 and ov_cephei_coordinates.size !=0: #Checks if all the stars were found
-      polaris_yildun_dis = np.linalg.norm(polaris_coordinates - yildun_coordinates)
-      polaris_ov_cephei_dis = np.linalg.norm(polaris_coordinates - ov_cephei_coordinates)
-      yildun_ov_cephei_dis = np.linalg.norm(yildun_coordinates - ov_cephei_coordinates)
+    ncp_coordinates, ncp_error = get_ncp_position(polaris_coordinates, yildun_coordinates, ov_cephei_coordinates, polaris_yildun_dis, polaris_ov_cephei_dis, yildun_ov_cephei_dis, image)
 
-      allStarsFound = True
-      #print("\nPolaris Yildun distance in pixels:", f"{polaris_yildun_dis:.2f}")
-      #print("Polaris OV Cephei distance in pixels:", f"{polaris_ov_cephei_dis:.2f}")
-      #print("Yildun OV Cephei distance in pixels:", f"{yildun_ov_cephei_dis:.2f}")
+    print("\nNCP coordinates:", ncp_coordinates)
+    print("NCP error from img center in arcmin:", ncp_error)
 
-      ncp_coordinates, ncp_error = get_ncp_position(polaris_coordinates, yildun_coordinates, ov_cephei_coordinates, polaris_yildun_dis, polaris_ov_cephei_dis, yildun_ov_cephei_dis, image)
+    draw_ncp(ncp_coordinates, polaris_coordinates, yildun_coordinates, ov_cephei_coordinates, image)
 
-      print("\nNCP coordinates:", ncp_coordinates)
-      print("NCP error from img center in arcmin:", ncp_error)
+  elif polaris_coordinates.size !=0 and (yildun_coordinates.size == 0 or ov_cephei_coordinates.size == 0): #Polaris found but cannot trilaterate - insufficent stars found
+    image_center = np.array([image.shape[1] // 2, image.shape[0] // 2])
+    ncp_error = (image_center - polaris_coordinates)/default_pix_per_arcmin
+    allStarsFound = False
 
-      draw_ncp(ncp_coordinates, polaris_coordinates, yildun_coordinates, ov_cephei_coordinates, image)
-
-    elif polaris_coordinates.size !=0 and (yildun_coordinates.size == 0 or ov_cephei_coordinates.size == 0): #Polaris found but cannot trilaterate - insufficent stars found
-      image_center = np.array([image.shape[1] // 2, image.shape[0] // 2])
-      ncp_error = (image_center - polaris_coordinates)/default_pix_per_arcmin
-      allStarsFound = False
-
-    else:
-      print("Polaris or other stars not found")
-      allStarsFound = False
+  else:
+    print("Polaris or other stars not found")
+    allStarsFound = False
 
   #Function should return polarisFound (True/False), allStarsFound (True,False), NCP error (arcmin) - [0,0] if not found
   return ncp_error[0], ncp_error[1], polarisFound, allStarsFound
