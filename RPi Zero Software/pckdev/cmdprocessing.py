3910f572cc61f471e305811f6449ac4057a0748b
PecaJosef <pecka.jozka@seznam.cz>
2026-03-09 22:28:35 +0100

    Enabled Debug mode

--- RPi Zero Software/pckdev/cmdprocessing.py
+++ RPi Zero Software/pckdev/cmdprocessing.py
@@ -13,7 +13,7 @@ def alignmentError(uart):
 
     cv2.imwrite('stars.jpg', star_image)
 
-    NCPErrorX, NCPErrorY, polarisFound, ncpFound = getAlignmentError(star_image)
+    NCPErrorX, NCPErrorY, polarisFound, ncpFound = getAlignmentError(star_image, True)
     pae_msg = f"$PA:DEV:{int(ncpFound)}:{int(polarisFound)}:{-NCPErrorX:.5f}:{-NCPErrorY:.5f}"
     uart.send(pae_msg)
     return
